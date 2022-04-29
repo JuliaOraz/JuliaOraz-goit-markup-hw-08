f60f55e583997ac6d186d5f6f4ec29fb651447ca
JuliaOraz <uliaoraz@gmail.com>
2022-04-29 14:30:00 +0300

    Вносит отзывчивые изображения

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,12 +1,13 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
-​
+
   menuBtnRef.addEventListener("click", () => {
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
     menuBtnRef.classList.toggle("is-active");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
     mobileMenuRef.classList.toggle("is-open");
+    document.body.classList.toggle("menu_open");
   });
 })();
